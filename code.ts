dea1a46e57f1d903dc81619d3c617a250ca45ce8
fernando.junior <fernando.junior@informata.com.br>
2023-07-12 18:00:44 -0300

    :sparkles: cria linhas entre componentes

--- code.ts
+++ code.ts
@@ -12,8 +12,6 @@ figma.ui.onmessage = async msg => {
     await figma.loadFontAsync({ family: 'Inter', style: 'Medium' });
 
     const lines: LineNode[] = [];
-    const startCircles: EllipseNode[] = [];
-    const arrows: PolygonNode[] = [];
     const textNodes: TextNode[] = [];
 
     const lineType = msg.lineType === 'dashed' ? 'dashed' : 'solid';
@@ -42,23 +40,6 @@ figma.ui.onmessage = async msg => {
       group.appendChild(line);
       lines.push(line);
 
-      const startCircle = figma.createEllipse();
-      startCircle.resize(45, 45);
-      startCircle.fills = line.strokes;
-      startCircle.x = startNode.x + startNode.width - startCircle.width / 2;
-      startCircle.y = startNode.y + startNode.height / 2 - 7 - startCircle.height / 2;
-      group.appendChild(startCircle);
-      startCircles.push(startCircle);
-
-      const arrow = figma.createPolygon();
-      arrow.resize(80, 80); // Ajuste o tamanho da flecha aqui
-      arrow.rotation = Math.atan2(endNode.y - startNode.y - startNode.height / 2, endNode.x - startNode.x - startNode.width) * (180 / Math.PI) + 89;
-      arrow.x = endNode.x - arrow.width / 2 - 59;
-      arrow.y = endNode.y + endNode.height / 2 - 30;
-      arrow.fills = line.strokes;
-      group.appendChild(arrow);
-      arrows.push(arrow);
-
       const textNode = figma.createText();
       textNode.fontName = { family: 'Inter', style: 'Medium' };
       textNode.characters = msg.lineName || '';
@@ -75,44 +56,26 @@ figma.ui.onmessage = async msg => {
     const updateLinePositions = () => {
       for (let i = 0; i < lines.length; i++) {
         const line = lines[i];
-        const startCircle = startCircles[i];
-        const arrow = arrows[i];
         const textNode = textNodes[i];
         const startNode = nodes[i] as FrameNode;
         const endNode = nodes[i + 1] as FrameNode;
 
-        const lineWidth = Math.max(endNode.x - startNode.x - startNode.width - arrow.width / 2, 0.01);
+        const lineWidth = Math.max(endNode.x - startNode.x - startNode.width, 2, 0.01);
         line.resizeWithoutConstraints(lineWidth, 0);
         line.x = startNode.x + startNode.width;
         line.y = startNode.y + startNode.height / 2;
-
-        startCircle.x = startNode.x + startNode.width - startCircle.width / 2;
-        startCircle.y = startNode.y + startNode.height / 2 - 7 - startCircle.height / 2;
-
-        arrow.rotation = Math.atan2(endNode.y - startNode.y - startNode.height / 2, endNode.x - startNode.x - startNode.width) * (180 / Math.PI) + 89;
-        arrow.x = endNode.x - arrow.width / 2 - 59;
-        arrow.y = endNode.y + endNode.height / 2 - arrow.height / 3.8;
 
         textNode.x = line.x + line.width / 2 - textNode.width / 2;
         textNode.y = line.y - textNode.height / 2 - 65;
       }
     };
 
-    // Registrar evento de atualização das posições das linhas quando a seleção mudar ou um componente for movido
+    // Registrar updateLinePositions para ser chamado em selectionchange e documentchange
     figma.on('selectionchange', updateLinePositions);
     figma.on('documentchange', updateLinePositions);
-
-    // Fechar o plugin
-    figma.ui.onmessage = msg => {
-      if (msg.type === 'close-plugin') {
-        figma.ui.close();
-      }
-
-    };
   }
 };
 
 function getLineStrokes(color: RGB): ReadonlyArray<Paint> {
   return [{ type: 'SOLID', color }];
 }
-
